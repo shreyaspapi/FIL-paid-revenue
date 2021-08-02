10a9f000124dcc5744f5fa5305bd082c813f15a9
shreyas <spapinwar@gmail.com>
2021-08-02 11:25:36 +0530

    updated script

--- fetch.py
+++ fetch.py
@@ -1,11 +1,12 @@
 import requests
 import time
+import csv
 
 endpoint = "https://filfox.info/api/v1/address/f05/messages?pageSize=100&page={}".format
 
-revenue = 0
+field_names= ['cid', 'height', 'timestamp', 'from', 'to', 'nonce', 'value', 'method', 'receipt']
 
-for i in range(10000):
+for i in range(3500, 10000):
     print("i = {}".format(i))
     response = dict(requests.get(endpoint(i)).json())
     try:
@@ -18,13 +19,7 @@ for i in range(10000):
         response = dict(requests.get(endpoint(i)).json())
         messages = response["messages"]
         
-
-    for msg in messages:
-        day = time.strftime('%d', time.localtime(msg["timestamp"]))
-        if day == 19:
-            print("I am at 19")
-            print(revenue)
-            break
-
-        if day == 20:
-            revenue += (msg["value"] / (10 ** 18))
+    with open('response_{}.csv'.format(i), 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
+        writer.writeheader()
+        writer.writerows(messages)
